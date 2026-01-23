a9c849747f2e0088e661d4565b1e005f1efd5f23
Ayush Khatiwada <ayush7khatiwada@gmail.com>
2026-01-23 16:21:14 +0000

    save

--- vector.cpp
+++ vector.cpp
@@ -47,8 +47,67 @@ std::vector<int> create_fibonacci_vector(const int n) {
 }
 
 
+template<typename T, typename U>
+std::vector<T> scale_vector(const std::vector<T>& vector, const U& scalar) {
+    std::vector<T> output_vector;
+    output_vector.reserve(vector.size());
+    
+    for (const T& element : vector) {
+        output_vector.push_back(scalar * element);
+    }
+    return output_vector;
+}
+
+template <typename T, typename U>
+auto scale_vector2(const std::vector<T>& v, const U& scalar)
+    -> std::vector<decltype(T{} * scalar)>
+{
+    using R = decltype(T{} * scalar);
+    std::vector<R> result;
+    result.reserve(v.size());
+
+    for (const T& element : v)
+        result.push_back(scalar * element);
+
+    return result;
+}
+
+
+// Vectors of the same type
+template<typename T>
+std::vector<T> add_vectors(const std::vector<T>& v1, const std::vector<T>& v2) {
+    if (v1.size() != v2.size()) throw std::invalid_argument("Vectors must be of the same size");
+
+    std::vector<T> result;
+    result.reserve(v1.size());
+
+    for (std::size_t i = 0; i < v1.size(); i++) {
+        result.push_back(v1[i] + v2[i]);
+    }
+    return result;
+}
+
+template<typename T, typename U>
+auto add_vectors2(const std::vector<T>& v1, const std::vector<U>& v2) 
+    -> std::vector<decltype(T{} + U{})>
+{
+    if (v1.size() != v2.size()) throw std::invalid_argument("Vectors must be of the same size");
+
+    using R = decltype(T{} + U{});
+    std::vector<R> result(v1.size());
+
+    for (std::size_t i = 0; i < v1.size(); i++) {
+        result[i] = v1[i] + v2[i];
+    }
+    return result;
+}
+
+
+
 int main() {
-    std::cout << create_fibonacci_vector(5) << std::endl;
+    std::vector<int> v1 = {1, 2, 3};
+    std::vector<double> v2 = {4, 5, 6};
+    std::cout << add_vectors2(v1, v2) << std::endl;
 
     return 0;
 }
